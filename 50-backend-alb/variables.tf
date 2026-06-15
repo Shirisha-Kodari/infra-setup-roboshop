dddeb27c5b393d6edddb7b1843bbe8aedace9446
shirishakodari <kodarishirisha419@gmail.com>
2026-06-15 16:21:50 +0530

    infra-setup-roboshop

---
+++ 50-backend-alb/variables.tf
@@ -0,0 +1,16 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z0342386ADU8CPI7S8K"
+}
+
+variable "zone_name" {
+    default = "kodari.online"
+}
+  
---
+++ 85-catalogue/variables.tf
@@ -0,0 +1,25 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+
+# variable "zone_id" {
+#     default = "Z04443413B38PRB5H0TW0"
+# }
+
+# variable "zone_name" {
+#     default = "daws84s.cfd"
+# }
+
+
+variable "zone_id" {
+    default = "Z0342386ADU8CPI7S8K"
+}
+
+variable "zone_name" {
+    default = "kodari.online"
+}
