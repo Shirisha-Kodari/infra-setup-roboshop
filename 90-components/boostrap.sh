a50f07ae50b8ebfa5ede9df34df2d506a61bba22
shirishakodari <kodarishirisha419@gmail.com>
2026-06-15 17:26:46 +0530

    update

--- 90-components/boostrap.sh
+++ 90-components/boostrap.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-component=$1
-dnf install ansible -y
-ansible-pull -U https://github.com/Shirisha-Kodari/ansible-roboshop-roles-tf.git -e component=$1 -e env=$2 main.yaml
+# component=$1
+# dnf install ansible -y
+# ansible-pull -U https://github.com/Shirisha-Kodari/ansible-roboshop-roles-tf.git -e component=$1 -e env=$2 main.yaml
+
+
+
+cd /var
+sudo mkdir roboshop
+sudo chown ec2-user:ec2-user roboshop
+cd roboshop
+git clone https://github.com/Shirisha-Kodari/infra-setup-roboshop.git
+cd infra-setup-roboshop/90-components
+terraform init
